5c7c97fa6daa7a5a06e4b61ebc22d34d62fd0972
Joel Singh <joelsingh788@gmail.com>
2024-12-02 14:49:24 -0500

    Make somewhat readable colorscheme

--- lua/lush_theme/pink-panic-new-syntax.lua
+++ lua/lush_theme/pink-panic-new-syntax.lua
@@ -67,9 +67,36 @@ local theme = lush(function(injected_functions)
     -- lCursor        { }, -- Character under the cursor when |language-mapping| is used (see 'guicursor')
     -- CursorIM       { }, -- Like Cursor, but used when in IME mode |CursorIM|
     -- CursorColumn   { }, -- Screen-column at the cursor, when 'cursorcolumn' is set.
+
     Normal { bg = hsl(330, 100, 93), fg = hsl(285, 90, 25) }, -- Normal text
     CursorLine { bg = Normal.bg.li(30) }, -- Screen-line at the cursor, when 'cursorline' is set. Low-priority if foreground (ctermfg OR guifg) is not set.
-    -- Directory      { }, -- Directory names (and other special names in listings)
+    Folded { bg = Normal.bg.da(10) }, -- Line used for closed folds
+    CursorColumn { CursorLine }, -- Screen-column at the cursor, when 'cursorcolumn' is set.
+    Search { fg = Normal.fg.da(10), bg = hsl(283, 80, 80) }, -- Last search pattern highlighting (see 'hlsearch'). Also used for similar items that need to stand out.
+    Substitute { Search }, -- |:substitute| replacement text highlighting
+    VertSplit { bg = Search.fg }, -- Column separating vertically split windows
+    IncSearch { Search }, -- 'incsearch' highlighting; also used for the text replaced with ":s///c"
+    CursorLineNr { fg = Folded.bg.da(20), gui = 'bold' }, -- Like LineNr when 'cursorline' or 'relativenumber' is set for the cursor line.
+    LineNr { fg = CursorLineNr.fg.li(40) }, -- Line number for ":number" and ":#" commands, and when 'number' or 'relativenumber' option is set.
+    MatchParen { fg = hsl('#11A19C').li(50), bg = hsl '#11A19C' }, -- Character under the cursor or just before it, if it is a paired bracket, and its match. |pi_paren.txt|
+    Visual { fg = hsl '#A9331B', bg = hsl '#FD7096' }, -- Visual mode selection
+    Winseparator { fg = Folded.bg.da(2) }, -- Separator between window splits. Inherts from |hl-VertSplit| by default, which it will replace eventually.
+    SignColumn { bg = Normal.bg.da(3) }, -- Column where |signs| are displayed
+    Whitespace { LineNr }, -- "nbsp", "space", "tab" and "trail" in 'listchars'
+    EndOfBuffer { LineNr }, -- Filler lines (~) after the end of the buffer. By default, this is highlighted like |hl-NonText|.
+    Directory { fg = Visual.bg }, -- Directory names (and other special names in listings)
+
+    StatusLine { bg = Normal.bg }, -- Status line of current window
+    StatusLineNC {}, -- Status lines of not-current windows. Note: If this is equal to "StatusLine" Vim will use "^^^" in the status line of the current window.
+    NormalFloat { bg = Folded.bg.li(20) }, -- Normal text in floating windows.
+    Pmenu { bg = Folded.bg.li(20) }, -- Popup menu: Normal item.
+    PmenuSel { bg = Pmenu.bg.li(20) }, -- Popup menu: Selected item.
+    TabLineSel { Visual }, -- Tab pages line, active tab page label
+    DiffDelete { fg = hsl('#FF70B5').da(15) }, -- Diff mode: Deleted line |diff.txt|
+    DiffAdd { fg = DiffDelete.fg.desaturate(60).da(15) }, -- Diff mode: Added line |diff.txt|
+    DiffChange { fg = DiffDelete.fg.saturate(82).li(30) }, -- Diff mode: Changed line |diff.txt|
+
+    -- Directory      {}, -- Directory names (and other special names in listings)
     -- DiffAdd        { }, -- Diff mode: Added line |diff.txt|
     -- DiffChange     { }, -- Diff mode: Changed line |diff.txt|
     -- DiffDelete     { }, -- Diff mode: Deleted line |diff.txt|
@@ -141,6 +168,47 @@ local theme = lush(function(injected_functions)
 
     -- Comment        { }, -- Any comment
 
+    Constant { fg = hsl('#CDF0EA').rotate(160).da(30) }, -- (*) Any constant
+    -- String         { }, --   A string constant: "this is a string"
+    -- Character      { }, --   A character constant: 'c', '\n'
+    Number { fg = Normal.fg.rotate(18).li(30) }, --   A number constant: 234, 0xff
+    -- Boolean        { }, --   A boolean constant: TRUE, false
+    -- Float          { }, --   A floating point constant: 2.3e10
+
+    Identifier { fg = Normal.fg.li(35) }, -- (*) Any variable name
+    -- Function       { }, --   Function name (also: methods for classes)
+
+    Statement { fg = hsl '#E90C7A' }, -- (*) Any statement
+    -- Conditional    { }, --   if, then, else, endif, switch, etc.
+    -- Repeat         { }, --   for, do, while, etc.
+    Label { CursorLineNr }, --   case, default, etc.
+    Operator { bg = Normal.bg.da(10) }, --   "sizeof", "+", "*", etc.
+    -- Keyword        { }, --   any other keyword
+    -- Exception      { }, --   try, catch, throw
+
+    PreProc { fg = hsl('#FFAAF5').da(20) }, -- (*) Generic Preprocessor
+    -- Include        { }, --   Preprocessor #include
+    -- Define         { }, --   Preprocessor #define
+    -- Macro          { }, --   Same as Define
+    -- PreCondit      { }, --   Preprocessor #if, #else, #endif, etc.
+
+    Type { fg = hsl '#E90CDE' }, -- (*) int, long, char, etc.
+    -- StorageClass   { }, --   static, register, volatile, etc.
+    -- Structure      { }, --   struct, union, enum, etc.
+    -- Typedef        { }, --   A typedef
+
+    Special { fg = Visual.fg }, -- (*) Any special symbol
+    -- SpecialChar    { }, --   Special character in a constant
+    -- Tag            { }, --   You can use CTRL-] on this
+    -- Delimiter      { }, --   Character that needs attention
+    -- SpecialComment { }, --   Special things inside a comment (e.g. '\n')
+    -- Debug          { }, --   Debugging statements
+
+    Underlined { gui = 'underline, italic' }, -- Text that stands out, HTML links
+    -- Ignore         { }, -- Left blank, hidden |hl-Ignore| (NOTE: May be invisible here in template)
+    -- Error          { }, -- Any erroneous construct
+    -- Todo           { }, -- Anything that needs extra attention; mostly the keywords TODO FIXME and XXX
+
     -- Constant       { }, -- (*) Any constant
     -- String         { }, --   A string constant: "this is a string"
     -- Character      { }, --   A character constant: 'c', '\n'
@@ -196,7 +264,20 @@ local theme = lush(function(injected_functions)
     -- LspSignatureActiveParameter { } , -- Used to highlight the active parameter in the signature help. See |vim.lsp.handlers.signature_help()|.
 
     -- See :h diagnostic-highlights, some groups may not be listed, submit a PR fix to lush-template!
-    --
+
+    DiagnosticError { fg = hsl '#FF5301' }, -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+    DiagnosticWarn { fg = hsl '#015892' }, -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+    DiagnosticInfo { fg = hsl '#B4AEE8' }, -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+    DiagnosticHint { fg = hsl('#FFC23D').da(35) }, -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+    -- DiagnosticVirtualTextError { } , -- Used for "Error" diagnostic virtual text.
+    -- DiagnosticVirtualTextWarn  { } , -- Used for "Warn" diagnostic virtual text.
+    -- DiagnosticVirtualTextInfo  { } , -- Used for "Info" diagnostic virtual text.
+    -- DiagnosticVirtualTextHint  { } , -- Used for "Hint" diagnostic virtual text.
+    DiagnosticUnderlineError { sp = DiagnosticError.fg, gui = 'underline' }, -- Used to underline "Error" diagnostics.
+    DiagnosticUnderlineWarn { sp = DiagnosticWarn.fg.li(20), gui = 'underline' }, -- Used to underline "Warn" diagnostics.
+    DiagnosticUnderlineInfo { sp = DiagnosticInfo.fg, gui = 'underline' }, -- Used to underline "Info" diagnostics.
+    DiagnosticUnderlineHint { sp = DiagnosticHint.fg, gui = 'underline' }, -- Used to underline "Hint" diagnostics.
+
     -- DiagnosticError            { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
     -- DiagnosticWarn             { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
     -- DiagnosticInfo             { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
@@ -240,51 +321,107 @@ local theme = lush(function(injected_functions)
     --
     -- For more information see https://github.com/rktjmp/lush.nvim/issues/109
 
-    -- sym"@text.literal"      { }, -- Comment
+    TSBoolean { fg = hsl(240, 100, 70), gui = 'bold' }, -- Boolean literals: `True` and `False` in Python.
+    TSComment { fg = Normal.fg.li(55), gui = 'italic' }, -- Line comments and block comments.
+    TSCharacter { Constant }, -- Character literals: `'a'` in C.
+    TSString { TSCharacter }, -- String literals.
+    TSConditional { fg = Normal.fg.rotate(45).li(30), gui = 'bold' }, -- Keywords related to conditionals: `if`, `when`, `cond`, etc.
+    TSConstant { fg = hsl('#9ADCFF').da(40), gui = 'italic' }, -- Constants identifiers. These might not be semantically constant. E.g. uppercase variables in Python.
+    TSKeywordFunction { fg = Normal.fg.rotate(18).li(30), gui = 'bold' }, -- Keywords used to define a function: `function` in Lua, `def` and `lambda` in Python.
+    TSDefine { TSKeywordFunction }, -- Preprocessor #define statements.
+    TSFloat { fg = TSKeywordFunction.fg.da(10), gui = 'italic' }, -- Floating-point number literals.
+    TSFunction { fg = TSConstant.fg.rotate(71).li(23), gui = 'italic' }, -- Function calls and definitions.
+    TSMethod { TSFunction }, -- Method calls and definitions.
+    TSFuncBuiltin { fg = hsl '#F43C86' }, -- Built-in functions: `print` in Lua.
+    TSKeyword { fg = TSKeywordFunction.fg.li(20) }, -- Keywords that don't fit into other categories.
+    TSNumber { TSFloat }, -- Numeric literals that don't fit into other categories.
+    TSOperator { fg = hsl('#CFFFFE').da(60) }, -- Binary or unary operators: `+`, and also `->` and `*` in C.
+    TSParameter { fg = TSConditional.fg.li(35) }, -- Parameters of a function.
+    TSPunctBracket { fg = Normal.bg.da(30), gui = 'bold' }, -- Brackets, braces, parentheses, etc.
+    TSRepeat { TSConditional }, -- Keywords related to loops: `for`, `while`, etc.
+    TSVariableBuiltin { fg = Normal.fg.li(35), gui = 'bold' }, -- Variable names defined by the language: `this` or `self` in Javascript.
+    TSTypeBuiltin { fg = TSCharacter.fg.li(20) }, -- Built-in types: `i32` in Rust.
+    TSType { TSRepeat }, -- Type (and class) definitions and annotations.
+    TSField { fg = TSDefine.fg.da(5) }, -- Object and struct fields.
+    TSInclude { fg = TSParameter.fg, gui = 'bold' }, -- File or module inclusion keywords: `#include` in C, `use` or `extern crate` in Rust.
+    TSConstructor { TSPunctBracket }, -- Constructor calls and definitions: `{}` in Lua, and Java constructors.
+    TSPunctDelimiter {}, -- Punctuation delimiters: Periods, commas, semicolons, etc.
+    TSConstBuiltin { fg = TSConstant.fg.da(25) }, -- Built-in constant values: `nil` in Lua.
+    TSStrong { fg = TSRepeat.fg, gui = 'bold' }, -- Text to be represented in bold.
+    TSEmphasis { fg = TSDefine.fg, gui = 'italic' }, -- Text to be represented with emphasis.
+    TSUnderline { fg = TSEmphasis.fg, gui = 'italic, underline' }, -- Text to be represented with an underline.
+    TSStrike { fg = TSStrong.fg, gui = 'strikethrough, bold' }, -- Strikethrough text.
+    TSAttribute { TSInclude }, -- Annotations that can be attached to the code to denote some kind of meta information. e.g. C++/Dart attributes.
+    TSCharacterSpecial { TSOperator }, -- Special characters.
+    TSConstMacro { TSConstructor }, -- Constants defined by macros: `NULL` in C.
+    TSException { TSConstBuiltin }, -- Exception related keywords: `try`, `except`, `finally` in Python.
+    TSKeywordOperator { TSConstructor }, -- Unary and binary operators that are English words: `and`, `or` in Python; `sizeof` in C.
+    TSKeywordReturn { TSStrong }, -- Keywords like `return` and `yield`.
+    TSStringEscape { fg = TSVariableBuiltin.fg.li(40) }, -- Escape characters within a string: `\n`, `\t`, etc.
+    TSStringSpecial { TSConstructor }, -- Strings with special meaning that don't fit into the previous categories.
+    TSSymbol { TSOperator }, -- Identifiers referring to symbols or atoms.
+    TSTag { fg = TSStrong.fg.da(20) }, -- Tags like HTML tag names.
+    TSTagAttribute { TSConstructor }, -- HTML tag attributes.
+    TSTagDelimiter { fg = TSTag.fg.rotate(180).da(30) }, -- Tag delimiters like `<` `>` `/`.
+    TSText { fg = Normal.fg.li(15) }, -- Non-structured text. Like text in a markup language.
+    TSTitle { TSStrong }, -- Text that is part of a title.
+    TSLiteral { TSText }, -- Literal or verbatim text.
+    TSURI { fg = TSException.fg, gui = 'underline, bold' }, -- URIs like hyperlinks or email addresses.
+    TSMath { fg = TSStringEscape.fg.rotate(29).da(50) }, -- Math environments like LaTeX's `$ ... $`
+    TSTextReference { fg = TSText.fg.rotate(10).li(20) }, -- Footnotes, text references, citations, etc.
+    TSEnvironment { TSText }, -- Text environments of markup languages.
+    TSEnvironmentName { TSStringSpecial }, -- Text/string indicating the type of text environment. Like the name of a `\begin` block in LaTeX.
+    TSNote { fg = TSStrong.fg }, -- Text representation of an informational note.
+    TSWarning { fg = TSNote.fg.rotate(70).li(10) }, -- Text representation of a warning note.
+    TSDanger { fg = TSNote.fg.rotate(20).li(10) }, -- Text representation of a danger note.
+
+    sym '@text' { TSText }, -- Comment
+    sym '@text.literal' { TSText }, -- Comment
     -- sym"@text.reference"    { }, -- Identifier
-    -- sym"@text.title"        { }, -- Title
-    -- sym"@text.uri"          { }, -- Underlined
+    sym '@text.title' { TSStrong }, -- Title
+    sym '@text.uri' { TSURI }, -- Underlined
     -- sym"@text.underline"    { }, -- Underlined
     -- sym"@text.todo"         { }, -- Todo
-    -- sym"@comment"           { }, -- Comment
-    -- sym"@punctuation"       { }, -- Delimiter
-    -- sym"@constant"          { }, -- Constant
-    -- sym"@constant.builtin"  { }, -- Special
+    sym '@comment' { TSComment }, -- Comment
+    sym '@punctuation' { TSPunctBracket }, -- Delimiter
+    sym '@punctuation.delimiter' { TSPunctBracket }, -- Delimiter
+    sym '@constant' { TSConstant }, -- Constant
+    sym '@constant.builtin' { TSConstBuiltin }, -- Special
     -- sym"@constant.macro"    { }, -- Define
-    -- sym"@define"            { }, -- Define
+    sym '@define' { TSKeywordFunction }, -- Define
     -- sym"@macro"             { }, -- Macro
-    -- sym"@string"            { }, -- String
-    -- sym"@string.escape"     { }, -- SpecialChar
-    -- sym"@string.special"    { }, -- SpecialChar
-    -- sym"@character"         { }, -- Character
-    -- sym"@character.special" { }, -- SpecialChar
-    -- sym"@number"            { }, -- Number
-    -- sym"@boolean"           { }, -- Boolean
-    -- sym"@float"             { }, -- Float
-    -- sym"@function"          { }, -- Function
-    -- sym"@function.builtin"  { }, -- Special
+    sym '@string' { TSCharacter }, -- String
+    sym '@string.escape' { TSStringEscape }, -- SpecialChar
+    sym '@string.special' { TSConstructor }, -- SpecialChar
+    sym '@character' { TSCharacter }, -- Character
+    sym '@character.special' { TSCharacterSpecial }, -- SpecialChar
+    sym '@number' { TSFloat }, -- Number
+    sym '@boolean' { TSBoolean }, -- Boolean
+    sym '@float' { TSFloat }, -- Float
+    sym '@function' { TSFunction }, -- Function
+    sym '@function.builtin' { TSFuncBuiltin }, -- Special
     -- sym"@function.macro"    { }, -- Macro
-    -- sym"@parameter"         { }, -- Identifier
-    -- sym"@method"            { }, -- Function
-    -- sym"@field"             { }, -- Identifier
+    sym '@parameter' { TSParameter }, -- Identifier
+    sym '@method' { TSMethod }, -- Function
+    sym '@field' { TSField }, -- Identifier
     -- sym"@property"          { }, -- Identifier
-    -- sym"@constructor"       { }, -- Special
-    -- sym"@conditional"       { }, -- Conditional
-    -- sym"@repeat"            { }, -- Repeat
+    sym '@constructor' { TSPunctBracket }, -- Special
+    sym '@conditional' { TSConditional }, -- Conditional
+    sym '@repeat' { TSRepeat }, -- Repeat
     -- sym"@label"             { }, -- Label
-    -- sym"@operator"          { }, -- Operator
-    -- sym"@keyword"           { }, -- Keyword
-    -- sym"@exception"         { }, -- Exception
-    -- sym"@variable"          { }, -- Identifier
-    -- sym"@type"              { }, -- Type
+    sym '@operator' { TSOperator }, -- Operator
+    sym '@keyword' { TSKeyword }, -- Keyword
+    sym '@exception' { TSException }, -- Exception
+    sym '@variable' { Identifier }, -- Identifier
+    sym '@type' { TSRepeat }, -- Type
     -- sym"@type.definition"   { }, -- Typedef
     -- sym"@storageclass"      { }, -- StorageClass
     -- sym"@structure"         { }, -- Structure
     -- sym"@namespace"         { }, -- Identifier
-    -- sym"@include"           { }, -- Include
+    sym '@include' { TSInclude }, -- Include
     -- sym"@preproc"           { }, -- PreProc
     -- sym"@debug"             { }, -- Debug
-    -- sym"@tag"               { }, -- Tag
+    sym '@tag' { TSTag }, -- Tag
   }
 end)
 
